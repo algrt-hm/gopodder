eef403a7c51e00d0556cc4bcfdea448f7b55602c
algrt-hm <46109874+algrt-hm@users.noreply.github.com>
2023-06-06 17:55:38 +0100

    Edit

--- gopodder_test.go
+++ gopodder_test.go
@@ -1,8 +1,37 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+func lastestPodsFromDb() int {
+	dbFileName := "/titanium/new_podcasts/gopodder.db"
+	db, err := sql.Open("sqlite3", dbFileName)
+	checkErr(err)
+
+	if err == nil {
+		fmt.Printf("Connected to db %s\n", dbFileName)
+		defer db.Close()
+	}
+
+	query := "select filename from downloads order by tagged_at desc limit 5"
+
+	rows, err := db.Query(query)
+	checkErr(err)
+
+	var count int
+	for rows.Next() {
+		err = rows.Scan(&count)
+		checkErr(err)
+		count += 1
+	}
+
+	return count
+}
 
 func TestCleanText(t *testing.T) {
 	s := "How €200bn of 'dirty money' flowed through a Danish bank Album: Behind the Money Genre: Podcast"
@@ -20,5 +49,13 @@ func TestCleanText(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, wanted %q", got, want)
 	}
+}
 
+func TestLastestPodsFromDb(t *testing.T) {
+	want := 5
+	got := lastestPodsFromDb()
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
 }
--- makefile
+++ makefile
@@ -1,6 +1,9 @@
 
 all: gopodder
 
+test:
+	go test -timeout 30s -run ^TestLastestPodsFromDb$$ gopodder
+
 gopodder:
 	go build gopodder.go
 
